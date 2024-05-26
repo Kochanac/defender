453824eb3469df6aa5078cf4f0e641dc208951a9
Костя Веселов <kveselov@ozon.ru>
2024-05-26 03:12:39 +0300

    upd

--- configs/tasks.sql
+++ configs/tasks.sql
@@ -18,3 +18,14 @@ VALUES (3, 'Defence 101', 'https://kochan.fun/f/defence-101.tar.gz', 'http://{ho
 insert into checker 
     (task_id, checker_url)
 VALUES (3, '../checkers/defence-101/checker.py');
+
+
+
+insert into tasks
+    (id, title, download_url, demo_url, flag, qemu_qcow2_path)
+VALUES (4, 'Wall', 'https://kochan.fun/f/wall.tar.gz', 'http://{host}:8080/', 'LyceumCTF{w3lc0m3_t0_d3f3nc3}', '/var/lib/libvirt/images/wall.qcow2');
+
+insert into checker 
+    (task_id, checker_url)
+VALUES (4, '../checkers/wall/wall_checker.py');
+
