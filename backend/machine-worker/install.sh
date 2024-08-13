953bf0470c55e1c705bafe3ec4b644e54d2f969a
Костя Веселов <kveselov@ozon.ru>
2024-08-13 20:29:36 +0300

    fix remove line

--- backend/machine-worker/install.sh
+++ backend/machine-worker/install.sh
@@ -4,5 +4,7 @@
 mkdir -p /etc/defender-worker/
 cp config.yaml /etc/defender-worker/config.yaml
 
+export CGO_ENABLED=1
+
 go build -o /sbin/defender-api cmd/api/main.go cmd/api/routes.go
 go build -o /sbin/defender-worker cmd/worker/main.go
